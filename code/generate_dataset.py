1f75570753b5bf247a524f2a457a710a53e365e4
TheHummel <jannisalsbach@gmail.com>
2025-04-02 00:35:04 +0200

    refactor generate_dataset and allow filters

--- code/generate_dataset.py
+++ code/generate_dataset.py
@@ -30,20 +30,31 @@ def convert_dataset_to_jsonl():
     print(f"Dataset prepared at {output_file}")
 
 
-def generate_dataset():
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    source_dir = os.path.join(script_dir, "..", "data", "repos", "scrapy", "scrapy")
-    output_dir = os.path.join(script_dir, "..", "data", "training", "completion")
+def generate_dataset(
+    source_dir: str, output_dir: str, file_types: list[str], filter: str = None
+):
+    """
+    Generate training dataset by extracting snippets from source code files
+
+    Args:
+        source_dir (str): Source directory containing source code files
+        output_dir (str): Output directory to save training snippets
+        file_types (list[str]): List of file extensions to process
+        filter (str, optional): Filter to exclude files containing this string. Defaults to None.
+    """
 
     if not os.path.isdir(source_dir):
         raise ValueError(f"Source directory {source_dir} not found")
     os.makedirs(output_dir, exist_ok=True)
 
-    count = 0
+    count_snippets = 0
+    count_files = 0
     all_snippets = []
     for root, _, files in os.walk(source_dir):
         for file in tqdm(files, desc="Processing files"):
-            if file.endswith(".py"):
+            if file.split(".")[-1] in file_types and (
+                filter is None or filter not in file.lower()
+            ):
                 path = os.path.join(root, file)
                 file_base = os.path.splitext(file)[0]
                 with open(path, "r", encoding="utf-8") as f:
@@ -51,23 +62,32 @@ def generate_dataset():
                 structural = extract_structural_snippets(
                     lines, num_snippets=50, max_prev_lines=10
                 )
-                assignments = extract_assignment_snippets(
-                    lines, num_snippets=50, max_prev_lines=10
-                )
+                # assignments = extract_assignment_snippets(
+                #     lines, num_snippets=50, max_prev_lines=10
+                # )
                 all_snippets.extend(
                     (file_base, prefix, target)
-                    for prefix, target in structural + assignments
+                    for prefix, target in structural  # + assignments
                 )
+                count_files += 1
 
     random.shuffle(all_snippets)
     for i, (file_base, prefix, target) in enumerate(all_snippets, 1):
         with open(f"{output_dir}/{file_base}_{i:03d}.txt", "w", encoding="utf-8") as f:
             f.write(f"Input: {prefix}\nOutput: {target}\n")
-        count += 1
+        count_snippets += 1
 
-    print(f"Generated {count} training snippets in {output_dir}")
+    print(
+        f"Generated {count_snippets} training snippets from {count_files} files in {output_dir}"
+    )
 
 
 if __name__ == "__main__":
-    # generate_dataset()
-    convert_dataset_to_jsonl()
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    source_dir = os.path.join(script_dir, "..", "data", "repos", "klicker-uzh", "apps")
+    output_dir = os.path.join(script_dir, "..", "data", "training", "completion")
+
+    generate_dataset(
+        source_dir, output_dir, file_types=["ts", "tsx"], filter="practicequiz"
+    )
+    # convert_dataset_to_jsonl()
