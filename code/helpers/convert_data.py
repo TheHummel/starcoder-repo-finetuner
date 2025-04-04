1f32e02b9e271e1278c9a76104b9ceec558d2a6c
TheHummel <jannisalsbach@gmail.com>
2025-04-04 17:00:31 +0200

    update placeholders

--- code/helpers/convert_data.py
+++ code/helpers/convert_data.py
@@ -12,8 +12,8 @@ def convert_dataset_to_jsonl(input_dir: str, output_file: str):
                     os.path.join(input_dir, filename), "r", encoding="utf-8"
                 ) as f_in:
                     text = f_in.read()
-                    prompt, completion = text.split("Output: ")
-                    prompt = prompt.replace("Input: ", "").strip()
+                    prompt, completion = text.split("_OUTPUT_: ")
+                    prompt = prompt.replace("_INPUT_: ", "").strip()
                     completion = completion.strip()
                     # combine prompt and completion with a separator
                     f_out.write(json.dumps({"text": f"{prompt}\n{completion}"}) + "\n")
