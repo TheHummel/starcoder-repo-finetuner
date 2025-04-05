2bd85f08efb3c15e61e86789e671893608917a2c
TheHummel <jannisalsbach@gmail.com>
2025-04-05 13:56:33 +0200

    update placeholders and include separator option

--- code/helpers/convert_data.py
+++ code/helpers/convert_data.py
@@ -2,7 +2,7 @@ import os
 import json
 
 
-def convert_dataset_to_jsonl(input_dir: str, output_file: str):
+def convert_dataset_to_jsonl(input_dir: str, output_file: str, separator: str = "\n"):
 
     os.makedirs(os.path.dirname(output_file), exist_ok=True)
     with open(output_file, "w", encoding="utf-8") as f_out:
@@ -12,10 +12,12 @@ def convert_dataset_to_jsonl(input_dir: str, output_file: str):
                     os.path.join(input_dir, filename), "r", encoding="utf-8"
                 ) as f_in:
                     text = f_in.read()
-                    prompt, completion = text.split("_OUTPUT_: ")
-                    prompt = prompt.replace("_INPUT_: ", "").strip()
+                    prompt, completion = text.split("__OUTPUT__: ")
+                    prompt = prompt.replace("__INPUT__: ", "").strip()
                     completion = completion.strip()
                     # combine prompt and completion with a separator
-                    f_out.write(json.dumps({"text": f"{prompt}\n{completion}"}) + "\n")
+                    f_out.write(
+                        json.dumps({"text": f"{prompt}{separator}{completion}"}) + "\n"
+                    )
 
     print(f"Dataset prepared at {output_file}")
