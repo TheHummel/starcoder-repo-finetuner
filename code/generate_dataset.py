d2306fce7f0e0336f29a5093a566fd912bfe8fc4
TheHummel <jannisalsbach@gmail.com>
2025-04-05 14:11:08 +0200

    feat: enhance dataset generation by supporting multiple source directories and improved filtering options

--- code/generate_dataset.py
+++ code/generate_dataset.py
@@ -5,69 +5,104 @@ from tqdm import tqdm
 from helpers.create_snippets import (
     extract_structural_snippets,
     extract_assignment_snippets,
+    extract_function_snippets_ts_full_each_line,
+    extract_function_snippets_ts,
 )
 from helpers.convert_data import convert_dataset_to_jsonl
 
 
 def generate_dataset(
-    source_dir: str, output_dir: str, file_types: list[str], filter: str = None
+    source_dirs: list[str],
+    output_dir: str,
+    file_types: list[str],
+    filters_out: list[str] = None,
+    filters_in: list[str] = None,
 ):
-    """
-    Generate training dataset by extracting snippets from source code files
-
-    Args:
-        source_dir (str): Source directory containing source code files
-        output_dir (str): Output directory to save training snippets
-        file_types (list[str]): List of file extensions to process
-        filter (str, optional): Filter to exclude files containing this string. Defaults to None.
-    """
-
-    if not os.path.isdir(source_dir):
-        raise ValueError(f"Source directory {source_dir} not found")
     os.makedirs(output_dir, exist_ok=True)
-
-    count_snippets = 0
-    count_files = 0
     all_snippets = []
-    for root, _, files in os.walk(source_dir):
-        for file in tqdm(files, desc="Processing files"):
-            if file.split(".")[-1] in file_types and (
-                filter is None or filter not in file.lower()
-            ):
-                path = os.path.join(root, file)
-                file_base = os.path.splitext(file)[0]
-                with open(path, "r", encoding="utf-8") as f:
-                    lines = f.readlines()
-                structural = extract_structural_snippets(
-                    lines, num_snippets=50, max_prev_lines=10
-                )
-                # assignments = extract_assignment_snippets(
-                #     lines, num_snippets=50, max_prev_lines=10
-                # )
-                all_snippets.extend(
-                    (file_base, prefix, target)
-                    for prefix, target in structural  # + assignments
-                )
-                count_files += 1
+
+    count_files = 0
+    for source_dir in source_dirs:
+        if not os.path.isdir(source_dir):
+            print(f"Warning: {source_dir} not found. Skipping.")
+            continue
+        for root, _, files in os.walk(source_dir):
+            for file in tqdm(files, desc=f"Processing files in {source_dir}"):
+                if (
+                    file.split(".")[-1] in file_types
+                    and (
+                        filters_out is None
+                        or all(f.lower() not in file.lower() for f in filters_out)
+                    )
+                    and (
+                        filters_in is None
+                        or any(f.lower() in file.lower() for f in filters_in)
+                    )
+                ):
+                    path = os.path.join(root, file)
+                    file_base = os.path.splitext(file)[0]
+                    with open(path, "r", encoding="utf-8") as f:
+                        lines = f.readlines()
+                    snippets = extract_function_snippets_ts_full_each_line(
+                        lines, file_base
+                    )
+                    all_snippets.extend(snippets)
+                    count_files += 1
 
     random.shuffle(all_snippets)
     for i, (file_base, prefix, target) in enumerate(all_snippets, 1):
         with open(f"{output_dir}/{file_base}_{i:03d}.txt", "w", encoding="utf-8") as f:
-            f.write(f"Input: {prefix}\nOutput: {target}\n")
-        count_snippets += 1
-
+            f.write(f"__INPUT__: {prefix}\n__OUTPUT__: {target}\n")
     print(
-        f"Generated {count_snippets} training snippets from {count_files} files in {output_dir}"
+        f"Generated {len(all_snippets)} snippets in {count_files} files in {output_dir}"
     )
 
 
 if __name__ == "__main__":
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    source_dir = os.path.join(script_dir, "..", "data", "repos", "klicker-uzh", "apps")
+    source_dirs = [
+        os.path.join(
+            script_dir,
+            "..",
+            "data",
+            "repos",
+            "klicker-uzh",
+            "apps",
+            "frontend-manage",
+            "src",
+        ),
+        os.path.join(
+            script_dir,
+            "..",
+            "data",
+            "repos",
+            "klicker-uzh",
+            "apps",
+            "frontend-pwa",
+            "src",
+        ),
+    ]
+    filters_out = [
+        "practicequiz",
+        "node_modules",
+        "dist",
+        "cache",
+        "test",
+        "lib",
+        "prisma",
+        "_app",
+        "2024",
+        "2025",
+        "seed",
+    ]
+    # filters_in = [
+    #     "microlearning",
+    # ]
+
     output_dir = os.path.join(script_dir, "..", "data", "training", "completion")
 
     generate_dataset(
-        source_dir, output_dir, file_types=["ts", "tsx"], filter="practicequiz"
+        source_dirs, output_dir, file_types=["ts", "tsx"], filters_in=filters_out
     )
 
     input_dir = output_dir
--- code/generate_evaluation_set.py
+++ code/generate_evaluation_set.py
@@ -4,54 +4,65 @@ from tqdm import tqdm
 from helpers.create_snippets import (
     extract_structural_snippets,
     extract_assignment_snippets,
+    extract_function_snippets_ts_full_each_line,
 )
 from helpers.convert_data import convert_dataset_to_jsonl
 
 
 def generate_evaluation_set(
-    source_dir: str, output_dir: str, file_types: list[str], filter: str = None
+    source_dirs: list[str],
+    output_dir: str,
+    file_types: list[str],
+    filters_out: list[str] = None,
+    filters_in: list[str] = None,
 ):
     """
     Generate evaluation snippets by extracting snippets from source code files
 
     Args:
-        source_dir (str): Source directory containing source code files
+        source_dirs (list[str]): List of source directories containing source code files
         output_dir (str): Output directory to save evaluation snippets
         file_types (list[str]): List of file extensions to process
         filter (str, optional): Filter to include files containing this string. Defaults to None.
     """
-
-    if not os.path.isdir(source_dir):
-        raise ValueError(f"Source directory {source_dir} not found")
     os.makedirs(output_dir, exist_ok=True)
-
-    count_snippets = 0
-    count_files = 0
     all_snippets = []
-    for root, _, files in os.walk(source_dir):
-        for file in tqdm(files, desc="Processing files"):
-            if file.split(".")[-1] in file_types and (
-                filter is None or filter in file.lower()
-            ):
-                path = os.path.join(root, file)
-                file_base = os.path.splitext(file)[0]
-                project = path.split("projects/")[-1].split("/")[0]
-                with open(path, "r", encoding="utf-8") as f:
-                    lines = f.readlines()
-                structural = extract_structural_snippets(lines, max_prev_lines=10)
-                # assignments = extract_assignment_snippets(lines, max_prev_lines=10)
-                all_snippets.extend(
-                    (project, file_base, prefix, target)
-                    for prefix, target in structural  # + assignments
-                )
-                count_files += 1
+    count_files = 0
+
+    for source_dir in source_dirs:
+        if not os.path.isdir(source_dir):
+            print(f"Warning: {source_dir} not found. Skipping.")
+            continue
+        for root, _, files in os.walk(source_dir):
+            for file in tqdm(files, desc=f"Processing files in {source_dir}"):
+                if (
+                    file.split(".")[-1] in file_types
+                    and (
+                        filters_out is None
+                        or all(f.lower() not in file.lower() for f in filters_out)
+                    )
+                    and (
+                        filters_in is None
+                        or any(f.lower() in file.lower() for f in filters_in)
+                    )
+                ):
+                    path = os.path.join(root, file)
+                    file_base = os.path.splitext(file)[0]
+                    with open(path, "r", encoding="utf-8") as f:
+                        lines = f.readlines()
+                    snippets = extract_function_snippets_ts_full_each_line(
+                        lines, file_base
+                    )
+                    all_snippets.extend(
+                        (file_base, prefix, target) for _, prefix, target in snippets
+                    )
+                    count_files += 1
 
     random.shuffle(all_snippets)
-    for i, (project, file_base, prefix, target) in enumerate(all_snippets, 1):
-        with open(
-            f"{output_dir}/{project}_{file_base}_{i:03d}.txt", "w", encoding="utf-8"
-        ) as f:
-            f.write(f"Input: {prefix}\nOutput: {target}\n")
+    count_snippets = 0
+    for i, (file_base, prefix, target) in enumerate(all_snippets, 1):
+        with open(f"{output_dir}/{file_base}_{i:03d}.txt", "w", encoding="utf-8") as f:
+            f.write(f"__INPUT__: {prefix}\n__OUTPUT__: {target}\n")
         count_snippets += 1
 
     print(
@@ -61,15 +72,20 @@ def generate_evaluation_set(
 
 if __name__ == "__main__":
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    source_dir = os.path.join(current_dir, "..", "data", "repos", "klicker-uzh", "apps")
+    source_dirs = [
+        os.path.join(current_dir, "..", "data", "repos", "klicker-uzh", "apps"),
+        os.path.join(current_dir, "..", "data", "repos", "klicker-uzh", "packages"),
+    ]
     output_dir = os.path.join(current_dir, "..", "data", "evaluation", "snippets")
 
+    filters_in = ["practicequiz"]
+
     generate_evaluation_set(
-        source_dir, output_dir, file_types=["ts", "tsx"], filter="practicequiz"
+        source_dirs, output_dir, file_types=["ts", "tsx"], filters_in=filters_in
     )
 
     input_dir = output_dir
     output_file = os.path.join(
         current_dir, "..", "data", "evaluation", "evaluation.jsonl"
     )
-    convert_dataset_to_jsonl(input_dir, output_file)
+    convert_dataset_to_jsonl(input_dir, output_file, separator="__###__")
